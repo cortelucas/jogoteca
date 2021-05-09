6dc18e104bd745ea36628e4506151d355f0c22b4
Lucas S Corte <corte1994.lc@gmail.com>
2021-05-09 10:24:18 -0300

    correcao de codigo

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login'), proxima=url_for('novo')')
+        return redirect(url_for('login'), proxima=url_for('novo'))
     return render_template('novo.html', titulo="Novo Jogo")
 
 
